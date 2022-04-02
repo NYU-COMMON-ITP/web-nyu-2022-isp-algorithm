26f2eeff549e451b58ee15d35647b4c3e12e4b9a
AllenMok <yihan.mo@outlook.com>
2022-04-02 16:24:30 -0400

    all textbox on management pages works

--- pages/api/v1/managtSpaceSearch/index.js
+++ pages/api/v1/managtSpaceSearch/index.js
@@ -1,9 +1,10 @@
-import { getPropertiesbyId } from "../../../../src/data-access/searches";
+import { getSpacesforManagt } from "../../../../src/data-access/searches";
 
 export default async function handler(req, res) {
     try {
         const userSelection = req.body
-        const result = await getPropertiesbyId(userSelection.variables)
+        console.log('API')
+        const result = await getSpacesforManagt(userSelection.variables)
         console.log("result")
         console.log(result)
         res.send(JSON.stringify(result));
